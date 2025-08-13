199b614f00b5997208b99726fcb5f790b00f4e73
igoreditilioes <igorediti@gmail.com>
2025-08-13 00:16:09 -0300

    nova atualizacao

--- patrimonio/admin.py
+++ patrimonio/admin.py
@@ -4,7 +4,8 @@ from .models import Equipamento, Movimentacao
 @admin.register(Equipamento)
 class EquipamentoAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome', 'categoria', 'numero_patrimonio', 'status', 'funcionario', 'unidade')
-    search_fields = ('nome', 'numero_patrimonio', 'categoria__nome')
+    list_display_links = ('nome',)
+    search_fields = ('nome', 'numero_patrimonio', 'categoria__nome', 'status')
     list_filter = ('categoria', 'status', 'funcionario', 'unidade')
     ordering = ('nome',)
 
@@ -15,3 +16,4 @@ class MovimentacaoAdmin(admin.ModelAdmin):
     search_fields = ('equipamento__nome',)
     list_filter = ('data_movimentacao', 'origem_unidade', 'destino_unidade', 'origem_funcionario', 'destino_funcionario')
     ordering = ('-data_movimentacao',)
+    autocomplete_fields = ['equipamento', 'origem_funcionario', 'destino_funcionario', 'origem_unidade', 'destino_unidade']
